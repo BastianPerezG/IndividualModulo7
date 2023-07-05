d5ffae9aeb34633211903ec915da06f976b77c11
BastianPerezG <bastianesteban.perez@gmail.com>
2023-07-05 01:14:20 -0400

    agregando modelo y administracion

--- misTareas/tareasApp/admin.py
+++ misTareas/tareasApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from tareasApp.models import Etiqueta
 # Register your models here.
+
+class Etiquetas_Admin(admin.ModelAdmin):             # Modelo de empresas
+    list_display = ['nombre']
+    search_fields = ['nombre']
+    ordering = ['nombre']
+    fields = ['nombre']
+
+admin.site.register(Etiqueta, Etiquetas_Admin)
--- misTareas/tareasApp/models.py
+++ misTareas/tareasApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=45, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Etiqueta'
+        verbose_name_plural = 'Etiquetas'
+
+class Estado(models.Model):
+    nombre = models.CharField(max_length=45, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
+    class Meta:
+        verbose_name = 'Estado'
+        verbose_name_plural = 'Estados'
+
+class Tarea(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    etiqueta = models.ForeignKey(Etiqueta, on_delete=models.CASCADE, null=False, blank=False)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE, null=False, blank=False)
+    titulo = models.CharField(max_length=45, null=False, blank=False)
+    descripcion = models.CharField(max_length=150, null=False, blank=False)
+    fecha_vencimiento = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
+    
+    class Meta:
+        verbose_name = 'Tarea'
+        verbose_name_plural = 'Tareas'
